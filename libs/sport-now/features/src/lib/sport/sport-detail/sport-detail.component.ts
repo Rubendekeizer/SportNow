20389d3a9ffdc014e2fc3480dcccdec1fe24bb4e
rdKeizer <rubendekeizer@gmail.com>
2024-04-05 09:59:56 +0200

    backend stuff

---
+++ libs/sport-now/features/src/lib/sport/sport-detail/sport-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-sport-detail',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>sport-detail works!</p>`,
+  styles: ``,
+})
+export class SportDetailComponent {}
---
+++ libs/sport-now/features/src/lib/sport/sport-list/sport-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-sport-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sport-list.component.html',
+  styleUrl: './sport-list.component.css',
+})
+export class SportListComponent {}
